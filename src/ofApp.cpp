60ddccb8d8711ea4a50eabe38ff9069be2c35c5d
lauren mccarthy <laurmccarthy@gmail.com>
2014-08-07 17:33:36 -0400

    adding ui

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,61 +1,85 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup() {
+    gui = new ofxUICanvas();        // Create a canvas at (0,0) using the default width
+    
+    gui->addSlider("BACKGROUND",0.0,255.0,100.0);
+    gui->addToggle("FULLSCREEN", false);
+    
+    gui->autoSizeToFitWidgets();
+    ofAddListener(gui->newGUIEvent, this, &ofApp::guiEvent);
+    gui->loadSettings("settings.xml");
+    
+    ofShowCursor();
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::draw() {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::keyPressed(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+void ofApp::keyReleased(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
+void ofApp::mouseMoved(int x, int y ) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+void ofApp::mouseDragged(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+void ofApp::mousePressed(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+void ofApp::mouseReleased(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
+void ofApp::windowResized(int w, int h) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
+void ofApp::gotMessage(ofMessage msg) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
+void ofApp::dragEvent(ofDragInfo dragInfo) { 
 
 }
 
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::exit() {
+    gui->saveSettings("settings.xml");
+    delete gui;
+}
 
+void ofApp::guiEvent(ofxUIEventArgs &e) {
+    if(e.getName() == "BACKGROUND") {
+        ofxUISlider *slider = e.getSlider();
+        ofBackground(slider->getScaledValue());
+    } else if(e.getName() == "FULLSCREEN") {
+        ofxUIToggle *toggle = e.getToggle();
+        ofSetFullscreen(toggle->getValue());
+    }
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxUI.h"
 
 class ofApp : public ofBaseApp{
 
@@ -19,4 +20,8 @@ class ofApp : public ofBaseApp{
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
 		
+        void exit();
+        void guiEvent(ofxUIEventArgs &e);
+    
+        ofxUICanvas *gui;
 };
